a19854b107f3390fdc815c2893deb6e06dc6f9a0
Marcos F. Avendaño <marcosfavendano@gmail.com>
2022-12-02 10:03:05 +0100

    añado un comentario de codigo

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
         System.out.println("Hello world!");
 
         System.out.println("inicio proyecto");
+
+        System.out.println("añado un comentario del commit");
     }
 }
